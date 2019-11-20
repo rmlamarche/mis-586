f882c2019c308f037fa31ba1e815eac8be2eebaf
rmlamarche <rmlamarche@wpi.edu>
2019-11-20 13:50:04 -0500

    session

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const logger = require('./server/logger.js');
+const session = require('express-session');
 
 const PORT = process.env.PORT || 5860;
 
 app.listen(PORT, () => {
   logger.info(`mis-586 running on ${PORT}`);
 });
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'abc213',
+  cookie: {},
+  saveUninitialized: true,
+  resave: true
+}));
 
 app.use(require('./routes'));
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const { Router } =  require('express');
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  return res.send('TODO index.html');
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + req.session.views + '</p>');
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>');
+    res.end()
+  } else {
+    req.session.views = 1;
+    res.end('test memory session');
+  }
 });
 
 module.exports = router;
